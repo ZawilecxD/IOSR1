f76d59200f1fc315a73c9317c3c1dd6e4e06b4eb
ZawilecxD <zawilecxd@gmail.com>
2016-11-05 17:21:20 +0100

    external configuration setup, controller code modified, reading file is now possible

--- src/main/java/pl/agh/iosr/config/AppConfig.java
+++ src/main/java/pl/agh/iosr/config/AppConfig.java
@@ -3,7 +3,10 @@ package pl.agh.iosr.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -17,6 +20,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = { "pl.agh.iosr" })
+@PropertySource(value = "classpath:config.properties")
 public class AppConfig extends WebMvcConfigurerAdapter {
 
     @Override
@@ -37,17 +41,21 @@ public class AppConfig extends WebMvcConfigurerAdapter {
         return bean;
     }
 
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("config");
+        source.setUseCodeAsDefaultMessage(true);
+        return source;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
     @Bean(name = "multipartResolver")
     public CommonsMultipartResolver getMultipartResolver() {
         return new CommonsMultipartResolver();
     }
-
-    @Bean(name = "messageSource")
-    public ReloadableResourceBundleMessageSource getMessageSource() {
-        ReloadableResourceBundleMessageSource resource = new ReloadableResourceBundleMessageSource();
-        resource.setBasename("classpath:config");
-        resource.setDefaultEncoding("UTF-8");
-        return resource;
-    }
-
 }
--- src/main/java/pl/agh/iosr/controller/MainController.java
+++ src/main/java/pl/agh/iosr/controller/MainController.java
@@ -1,9 +1,12 @@
 package pl.agh.iosr.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
+import java.io.*;
 
 /**
  * Created by Murzynas on 2016-11-05.
@@ -11,6 +14,9 @@ import javax.annotation.PostConstruct;
 @RestController
 @RequestMapping("/")
 public class MainController {
+
+    @Value("${files.main.directory}")
+    private String filesDirectory;
 
     @PostConstruct
     public void init() {
@@ -29,14 +35,34 @@ public class MainController {
 
     @RequestMapping(value = "read/{fileName}", method = RequestMethod.GET)
     public @ResponseBody  String getUsersView(@PathVariable String fileName) {
-        System.out.println("READING FILE "+fileName);
-        return "GET OK";
+        String filePath = getPathToFile(fileName);
+        String returnText = "";
+        try {
+            FileReader fr = new FileReader(filePath);
+            BufferedReader br = new BufferedReader(fr);
+            String temp = "";
+            while((temp = br.readLine()) != null) {
+                returnText += temp;
+            }
+            fr.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("READING FILE "+filePath+", read text = "+returnText);
+        return returnText;
     }
 
     @RequestMapping(value = "write", method = RequestMethod.POST)
     public String getUsersView(@RequestBody String fileName, @RequestBody String text) {
-        System.out.println("WRITING TO FILE "+fileName + ", text = "+text);
+        System.out.println("WRITING TO FILE "+getPathToFile(fileName) + ", text = "+text);
         return "POST OK";
     }
 
+
+    private String getPathToFile(String fileName) {
+        return filesDirectory + "\\" + fileName + ".txt";
+    }
 }
