22732d887ca6c28294abb83b945805057bb0566d
swistaq <xswistaqx@gmail.com>
2016-11-15 15:24:22 +0100

    Added connection to ZooKeeper in manager and client. Separated client's pom from manager's.

--- Client/src/main/java/pl/agh/iosr/client/config/ClientRunner.java
+++ Client/src/main/java/pl/agh/iosr/client/config/ClientRunner.java
@@ -1,8 +1,13 @@
 package pl.agh.iosr.client.config;
 
-import java.io.FileNotFoundException;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
+import org.apache.curator.x.discovery.ServiceInstance;
+import org.apache.curator.x.discovery.UriSpec;
+
 import java.io.InputStream;
-import java.util.Properties;
 
 /**
  * Created by Murzynas on 2016-11-05.
@@ -10,11 +15,35 @@ import java.util.Properties;
 public class ClientRunner {
     private InputStream inputStream;
     public static String mainDirectoryPath;
+    private int port = 9001; //TODO: cmd
 
-    public static void main(String[] args) {
-        //TODO: rejestracja clienta w ZK
-        System.out.println(args[0]);
-        mainDirectoryPath = args[0];
+    public static void main(String[] args) throws Exception {
+
+        CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient("0.0.0.0:2181", new RetryNTimes(5, 1000));
+        curatorFramework.start();
+
+        ServiceInstance serviceInstance = ServiceInstance.builder()
+                .uriSpec(new UriSpec("{scheme}://{address}:{port}"))
+                .address("0.0.0.0")
+                .port(9001)
+                .name("worker")
+                .build();
+
+        ServiceDiscoveryBuilder.builder(Void.TYPE)
+                .basePath("iosr1")
+                .client(curatorFramework)
+                .thisInstance(serviceInstance)
+                .build()
+                .start();
+
+//        System.out.println(args[0]);
+//        mainDirectoryPath = args[0];
+
+        //kręcimy się w kółko
+        int i = 0;
+        while(true){
+            i++;
+        }
     }
 
     //TODO uzywamy properties czy arhumenty wywoalania progamu??
--- src/main/java/pl/agh/iosr/controller/ManagerController.java
+++ src/main/java/pl/agh/iosr/controller/ManagerController.java
@@ -1,5 +1,9 @@
 package pl.agh.iosr.controller;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.apache.curator.x.discovery.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
@@ -16,16 +20,48 @@ import java.io.*;
 @RequestMapping("/")
 public class ManagerController {
 
+    private ServiceProvider<Void> serviceProvider;
+
     @Value("${files.main.directory}")
     private String filesDirectory;
 
     @PostConstruct
     public void init() {
-        //TODO: initialize zookeeper for this manager
+        CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient("0.0.0.0:2181", new RetryNTimes(5, 1000));
+        curatorFramework.start();
+
+        ServiceDiscovery<Void> serviceDiscovery = ServiceDiscoveryBuilder.builder(Void.TYPE).basePath("iosr1").client(curatorFramework).build();
+        try {
+            serviceDiscovery.start();
+        } catch (Exception e) {
+            System.out.println("ERROR establishing Service Discovery. Aborting");
+            e.printStackTrace();
+            return;
+        }
+
+        serviceProvider = serviceDiscovery.serviceProviderBuilder().serviceName("worker").build();
+        try {
+            serviceProvider.start();
+        } catch (Exception e) {
+            System.out.println("ERROR establishing Service Provider. Aborting");
+            e.printStackTrace();
+            return;
+        }
     }
 
     @RequestMapping(value = "read/{fileName}", method = RequestMethod.GET)
     public @ResponseBody  String readFile(@PathVariable String fileName) {
+        ServiceInstance<Void> instance;
+        try {
+            instance = serviceProvider.getInstance();
+        } catch (Exception e) {
+            System.out.println("ERROR fetching instance. Aborting operation");
+            e.printStackTrace();
+            return null;
+        }
+        String address = instance.buildUriSpec();
+        String response;    //TODO: delegate work here
+
         String filePath = getPathToFile(fileName);
 
             //TODO: ten kod do klienta, a tutaj podbicie ZK o tresc do klienta
